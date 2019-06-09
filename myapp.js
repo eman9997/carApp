af87ef24585e2c1d33f5fe3954ee6881a83cd130
emanuel Fonseca <eman9997@yahoo.com>
2019-06-09 00:16:36 -0700

    dependency's installed

--- myapp.js
+++ myapp.js
@@ -5,6 +5,7 @@ let app= express();
 
 mongoose.connect("mongodb://localhost/car_app");
 app.set('view engine', 'ejs');
+// app.use(express.static("views"));
 
 let carSchema = new mongoose.Schema({
     make: String,
@@ -15,10 +16,10 @@ let carSchema = new mongoose.Schema({
 let Car =mongoose.model("Car", carSchema);
 
 Car.create({
-    make: "mazda",
-    model: "6",
+    make: "toyota",
+    model: "camery",
     description: "My First car",
-    image: "https://s1.poctra.com/700/copart-p-2018/m05/fc/fcd13182a25703b36d4cbaf5d7c4b4a4_2/2005-Mazda-6-S-front-left-30798168.jpg"
+    image: "https://i.ytimg.com/vi/2xgsPcMiWcY/maxresdefault.jpg"
 },(err,car)=>{
     if (err) {
         console.log("THis is your error  .."+err);
@@ -41,46 +42,6 @@ app.listen("3000", ()=>{
 
 
 
-
-
-
-
-
-
-
-// let express=require("express");
-// const app=express();
-// let searchterm;
-
-// const request=require("request");
-// app.use(express.static("public"));
-// // const requestPromises=require("request-promise-native");
-// app.set('view engine', 'ejs');
-
-// app.get("/",(req, res)=>{
-//     res.render("search");
-
-// });
-
-
-// app.get("/results",(req, res)=>{
-//     searchterm=req.query.Search;
-//     request('http://www.omdbapi.com/?s=' +searchterm+ '&apikey=thewdb', function (error, response, body) {
-//     if(!error && response.statusCode == 200){
-//         let data=JSON.parse(body);
-//         // console.log(data["Search"]["1"]["Title"]); 
-
-//         res.render("home",{data:data});
-//     }
-//     });
-    
-// });
-
-// // app.use(express.static("views"));
-
-// app.listen("3000",()=>{
-//     console.log("Now Serving localhost: 3000");
-// });
 
 
 
