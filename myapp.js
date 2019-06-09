5c2896a7a939816063b9519482666b4ecefe3b7f
emanuel Fonseca <eman9997@yahoo.com>
2019-06-09 01:00:24 -0700

    finished get, index page

--- myapp.js
+++ myapp.js
@@ -1,10 +1,12 @@
 let express =require("express");
 let mongoose =require("mongoose");
 let methodOverride=require('method-override');
+let bodyParser=require("body-parser");
 let app= express();
 
 mongoose.connect("mongodb://localhost/car_app");
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extened: true}));
 // app.use(express.static("views"));
 
 let carSchema = new mongoose.Schema({
@@ -15,20 +17,27 @@ let carSchema = new mongoose.Schema({
 });
 let Car =mongoose.model("Car", carSchema);
 
-Car.create({
-    make: "toyota",
-    model: "camery",
-    description: "My First car",
-    image: "https://i.ytimg.com/vi/2xgsPcMiWcY/maxresdefault.jpg"
-},(err,car)=>{
-    if (err) {
-        console.log("THis is your error  .."+err);
-    } else {
-        console.log(car);
-    }
-});
+// Car.create({
+//     make: "toyota",
+//     model: "camery",
+//     description: "My First car",
+//     image: "https://i.ytimg.com/vi/2xgsPcMiWcY/maxresdefault.jpg"
+// },(err,car)=>{
+//     if (err) {
+//         console.log("THis is your error  .."+err);
+//     } else {
+//         console.log(car);
+//     }
+// });
+
 app.get("/",(req,res)=>{
-    res.send("Welcome");
+    // if(error){
+    //     // console.log(err);
+    // } else{
+        Car.find({}, (err,cars)=>{
+            res.render("index",{cars:cars});
+        });
+//    }
 });
 
 
