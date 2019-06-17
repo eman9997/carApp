21b8a3b467d9a669776c06bd97818a2ee19b8351
emanuel Fonseca <eman9997@yahoo.com>
2019-06-16 20:16:48 -0700

    added new paths layed out foundations of app

--- myapp.js
+++ myapp.js
@@ -4,10 +4,12 @@ let methodOverride=require('method-override');
 let bodyParser=require("body-parser");
 let app= express();
 
-mongoose.connect("mongodb://localhost/car_app");
+
+mongoose.connect("mongodb://localhost/car_app",{useNewUrlParser: true } );
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extened: true}));
-// app.use(express.static("views"));
+app.use(express.static("public"));
 
 let carSchema = new mongoose.Schema({
     make: String,
@@ -16,6 +18,8 @@ let carSchema = new mongoose.Schema({
     image: String
 });
 let Car =mongoose.model("Car", carSchema);
+
+
 
 // Car.create({
 //     make: "toyota",
@@ -30,24 +34,54 @@ let Car =mongoose.model("Car", carSchema);
 //     }
 // });
 
+// INDEX PAGE
 app.get("/",(req,res)=>{
-    // if(error){
-    //     // console.log(err);
-    // } else{
-        Car.find({}, (err,cars)=>{
-            res.render("index",{cars:cars});
-        });
-//    }
+    res.redirect("/cars")
 });
+app.get("/cars",(req,res)=>{
+    Car.find({}, (err,cars)=>{
+        if(err){
+            // console.log(err);
+        } else{
+        res.render("index",{cars:cars});
+        }
+    });
+});
+// END OF INDEX
 
+
+// This new is part of posting Create then post
+app.get("/cars/new",(req,res)=>{
+    res.render("new");
+});
+app.post("/cars",(req,res)=>{
+    Car.create(req.body.car,(err,newcar)=>{
+        if (err) {
+            console.log("THis is your error  .."+err);
+        } else {
+            res.redirect("/cars");
+        }
+    });
+});
+// END of POST
+
+app.get('/cars/:id',(req,res)=>{
+    Car.findById(req.params.id,(err,foundCar)=>{
+        if(err){
+            console.log(err);
+        } else{
+            res.render("show",{car:foundCar});
+        }
+    });
+
+    
+});
 
 
 
 app.listen("3000", ()=>{
     console.log("Your Cars are on local Port 3000");
 });
-
-
 
 
 
